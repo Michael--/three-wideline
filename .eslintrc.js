46a2450f690d03df5c566ca4c0b89f1938177f80
M.Rieck <michael.rieck@de.neusoft.com>
2021-12-21 17:31:27 +0100

    update eslint config
    Change-Id: I38a3d55e52267e838c611bc3c2909e008e9aa6c7

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,13 +18,12 @@ module.exports = {
    ],
    plugins: ["react", "@typescript-eslint"],
    rules: {
-      "indent": ["error", 3],
+      "indent": ["error", 3, { SwitchCase: 1 }],
       "linebreak-style": ["error", "unix"],
       "quotes": ["error", "double"],
       "semi": ["error", "never"],
       "no-var": "error",
       "prefer-const": "error",
-      "indent": "off",
       "no-unused-vars": [
          "warn",
          {
