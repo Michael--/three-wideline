c6d2b53fef76b577ff073b7b4669cc83ae32735f
M.Rieck <michael.rieck@de.neusoft.com>
2021-12-22 14:43:06 +0100

    remove unused tsconfig.json, use only the 'named' versions of ts-config
    Change-Id: Ib26614027ed54557ff9ca87d7f20f6c71ee09d83

--- src/sample1/vite.config.js
+++ src/sample1/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import viteTsconfigPaths from "vite-tsconfig-paths"
 import rawPlugin from "vite-raw-plugin"
 
 export default defineConfig({
@@ -8,5 +7,5 @@ export default defineConfig({
       outDir: "build/sample1",
       chunkSizeWarningLimit: 1500,
    },
-   plugins: [react(), viteTsconfigPaths(), rawPlugin({ fileRegex: /\.(vs|fs)$/ })],
+   plugins: [react(), rawPlugin({ fileRegex: /\.(vs|fs)$/ })],
 })
