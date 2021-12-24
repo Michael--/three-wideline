9d84f793d0d0b27c94e8d770352e2486866b2ed5
M.Rieck <michael.rieck@de.neusoft.com>
2021-12-24 12:22:17 +0100

    add comment
    Change-Id: I365ea8dd77028c2c4bd9bb9dc3635135f1dbff9a

--- src/sample1/vite.config.js
+++ src/sample1/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import rawPlugin from "vite-raw-plugin"
+import rawPlugin from "vite-raw-plugin" // as an alternative "rollup-plugin-string" are also supported by vite
 
 export default defineConfig({
    build: {
