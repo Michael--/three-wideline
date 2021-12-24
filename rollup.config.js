1e95a86fb02a7bc626f2565a5e1f7f771c785b3b
M.Rieck <michael.rieck@de.neusoft.com>
2021-12-24 11:41:08 +0100

    config typings
    Change-Id: I8d4e08d7df8f5039686535dfc28e060452810c06

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,30 @@
-// rollup.config.js
+import { defineConfig } from "rollup"
 import typescript from "@rollup/plugin-typescript"
 import { string } from "rollup-plugin-string"
 import { apiExtractor } from "rollup-plugin-api-extractor"
 
-const config = [
-   {
-      input: "src/Wideline/index.tsx",
-      output: {
-         sourcemap: false,
-         dir: "dist",
-         format: "iife",
-         name: "ThreeWideline",
-         globals: {
-            "react": "React",
-            "react/jsx-runtime": "jsxRuntime",
-            "three": "three",
-         },
+const config = defineConfig({
+   input: "src/Wideline/index.tsx",
+   output: {
+      sourcemap: false,
+      dir: "dist",
+      format: "iife",
+      name: "ThreeWideline",
+      globals: {
+         "react": "React",
+         "react/jsx-runtime": "jsxRuntime",
+         "three": "three",
       },
-      external: ["react", "react/jsx-runtime", "three"],
-      plugins: [
-         typescript({ tsconfig: "tsconfig-dist.json" }),
-         string({ include: /\.(vs|fs)$/ }),
-         apiExtractor({
-            local: true,
-            configFile: "./api-extractor.json",
-         }),
-      ],
    },
-]
+   external: ["react", "react/jsx-runtime", "three"],
+   plugins: [
+      typescript({ tsconfig: "tsconfig-dist.json" }),
+      string({ include: /\.(vs|fs)$/ }),
+      apiExtractor({
+         local: true,
+         configFile: "./api-extractor.json",
+      }),
+   ],
+})
 
 export default config
