a1ed4c340979fc81198df9107041cf1826daf6fc
M.Rieck <michael.rieck@de.neusoft.com>
2021-12-27 14:38:42 +0100

    adjust eslint consig
    Change-Id: Ic12163c4e39d352e4bee61a58e55b08c2349b2c2

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
    extends: [
       "eslint:recommended",
       "plugin:react/recommended",
-      "plugin:react/jsx-runtime",
       "plugin:@typescript-eslint/recommended",
       "plugin:prettier/recommended",
    ],
