ed3e83f34c0577efd987c81fcb89e42933b3821e
M.Rieck <michael.rieck@de.neusoft.com>
2022-01-02 17:51:49 +0100

    use wouter and split sample in multiple pages
    Change-Id: I3c2f519e062a3de862fc0c7d2561f1d3e98abaca

--- src/sample1/Main.tsx
+++ src/sample1/Main.tsx
@@ -1,15 +1,14 @@
 import React from "react"
 import svglogo from "./logo.svg"
-import { Border } from "./Gui"
 import { SampleParts } from "./SampleParts"
 import { SampleMultiple } from "./SampleMultiple"
 import { CustomLineParts } from "./CustomLineParts"
 import { SampleConstruction } from "./SampleConstruction"
 import { SampleLogo } from "./SampleLogo"
 import { SampleLights } from "./SampleLights"
-import { Box, Text, Heading, Anchor, Sidebar, Nav } from "grommet"
+import { Box, Text, Heading, Anchor, Sidebar, Nav, Paragraph } from "grommet"
 import { name as pname, version } from "../../package.json"
-import { Logo } from "../Wideline"
+import { Route, Switch, useLocation } from "wouter"
 
 function Title() {
    const npmlink = "https://www.npmjs.com/package/three-wideline"
@@ -27,28 +26,78 @@ function Title() {
    )
 }
 
+interface IPage {
+   route: string
+}
+
+const pages: IPage[] = [
+   { route: "Logo" },
+   { route: "Parts" },
+   { route: "Construction" },
+   { route: "Custom" },
+   { route: "Lights" },
+   { route: "Multiple" },
+]
+
+const ActiveLink = (props: { href: string; children?: React.ReactNode }) => {
+   const [location, setLocation] = useLocation()
+   const isActive = location.includes(props.href)
+
+   return (
+      <div
+         {...props}
+         className={isActive ? "active" : ""}
+         onClick={() => {
+            setLocation(props.href ?? "")
+         }}
+      >
+         <a>{props.children}</a>
+         {isActive && <a>{` *${isActive}* ${location}`}</a>}
+      </div>
+   )
+}
+
 export function Main() {
+   const [location, setLocation] = useLocation()
+
+   React.useEffect(() => {
+      if (!location.includes(pages[0].route)) setLocation(pages[0].route)
+   }, [])
    return (
       <Box gap="small">
          <Title />
          <Box direction="row">
             <Sidebar background="brand" round="small" header={<img src={svglogo} />}>
                <Nav gap="small" border="all">
-                  <Text>DEPP</Text>
+                  {pages.map((e, i) => (
+                     <ActiveLink key={i} href={e.route}>
+                        {e.route}
+                     </ActiveLink>
+                  ))}
                </Nav>
             </Sidebar>
             <Box gap="small">
-               <SampleLogo />
-               <SampleParts />
-               <Border />
-               <CustomLineParts />
-               <Border />
-               <SampleConstruction />
-               <Border />
-               <SampleLights />
-               <Border />
-               <SampleMultiple />
-               <Border />
+               <Switch>
+                  <Route path="/Logo">
+                     <SampleLogo />
+                  </Route>
+                  <Route path="/Parts">
+                     <SampleParts />
+                  </Route>
+                  <Route path="/Construction">
+                     <SampleConstruction />
+                  </Route>
+                  <Route path="/Custom">
+                     <CustomLineParts />
+                  </Route>
+                  <Route path="/Lights">
+                     <SampleLights />
+                  </Route>
+                  <Route path="/Multiple">
+                     <SampleMultiple />
+                  </Route>
+                  <Route>404, Not Found!</Route>
+               </Switch>
             </Box>
          </Box>
       </Box>
