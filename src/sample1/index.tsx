4849c56743910ff385d9a03875cfa05034a93de6
M.Rieck <michael.rieck@de.neusoft.com>
2022-10-20 07:57:25 +0200

    adjust sample startup
    Change-Id: Iceffd7706f1579c708eb00270de5edfa3fd77259

--- src/sample1/index.tsx
+++ src/sample1/index.tsx
@@ -1,13 +1,13 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 import { App } from "./App"
 
 const start = () => {
-   const dom = (
+   const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+   root.render(
       <React.StrictMode>
          <App />
-      </React.StrictMode>
+      </React.StrictMode>,
    )
-   ReactDOM.render(dom, document.getElementById("root"))
 }
 export default start
