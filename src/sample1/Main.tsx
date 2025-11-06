71b67003cff5aa0384592fadf84d2e50e84c1df2
M.Rieck <mr@number10.de>
2025-11-06 11:18:09 +0100

    fix: import JSX
    Change-Id: Ib4996741a27348e7f2d9fb71f8bf4984712aceb3

--- src/sample1/Main.tsx
+++ src/sample1/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { JSX } from "react"
 import svglogo from "./logo.svg"
 import { SampleParts } from "./SampleParts"
 import { SampleMultiple } from "./SampleMultiple"
