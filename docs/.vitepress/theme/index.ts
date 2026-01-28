ba2d8cd76c4459332f45f17a793f102ba9577354
M.Rieck <mr@number10.de>
2026-01-28 14:22:56 +0100

    fix: missing type

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,11 @@
 import DefaultTheme from "vitepress/theme"
 import "./custom.css"
 import ExampleWideline from "./components/ExampleWideline.vue"
+import { EnhanceAppContext } from "vitepress"
 
 export default {
    ...DefaultTheme,
-   enhanceApp({ app }) {
+   enhanceApp({ app }: EnhanceAppContext) {
       app.component("ExampleWideline", ExampleWideline)
    },
 }
